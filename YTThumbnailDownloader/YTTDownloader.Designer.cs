67024d698688b9ee56c5f7567eaa6d09e4562bb8
LtSilence <33474569+LtSilence@users.noreply.github.com>
2020-07-14 16:08:11 +0200

    Add files via upload

---
+++ YTThumbnailDownloader/YTTDownloader.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace YTThumbnailDownloader {
+    partial class YTThumbnailDownloader {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(YTThumbnailDownloader));
+            this.label1 = new System.Windows.Forms.Label();
+            this.LinkBox = new System.Windows.Forms.TextBox();
+            this.thumbBox = new System.Windows.Forms.PictureBox();
+            this.DownloadButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.MsgLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.thumbBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(426, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LINK:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LinkBox
+            // 
+            this.LinkBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LinkBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.LinkBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinkBox.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LinkBox.ForeColor = System.Drawing.Color.White;
+            this.LinkBox.Location = new System.Drawing.Point(12, 39);
+            this.LinkBox.Name = "LinkBox";
+            this.LinkBox.Size = new System.Drawing.Size(910, 30);
+            this.LinkBox.TabIndex = 1;
+            this.LinkBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LinkBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.LinkBox_KeyDown);
+            // 
+            // thumbBox
+            // 
+            this.thumbBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.thumbBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.thumbBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thumbBox.InitialImage = null;
+            this.thumbBox.Location = new System.Drawing.Point(12, 123);
+            this.thumbBox.Name = "thumbBox";
+            this.thumbBox.Size = new System.Drawing.Size(910, 501);
+            this.thumbBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.thumbBox.TabIndex = 2;
+            this.thumbBox.TabStop = false;
+            // 
+            // DownloadButton
+            // 
+            this.DownloadButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.DownloadButton.FlatAppearance.BorderSize = 0;
+            this.DownloadButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownloadButton.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DownloadButton.ForeColor = System.Drawing.Color.White;
+            this.DownloadButton.Location = new System.Drawing.Point(393, 75);
+            this.DownloadButton.Name = "DownloadButton";
+            this.DownloadButton.Size = new System.Drawing.Size(151, 42);
+            this.DownloadButton.TabIndex = 3;
+            this.DownloadButton.Text = "DOWNLOAD";
+            this.DownloadButton.UseVisualStyleBackColor = true;
+            this.DownloadButton.Click += new System.EventHandler(this.DownloadButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.FlatAppearance.BorderColor = System.Drawing.SystemColors.WindowFrame;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Font = new System.Drawing.Font("Consolas", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.ForeColor = System.Drawing.Color.White;
+            this.SaveButton.Location = new System.Drawing.Point(771, 582);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(151, 42);
+            this.SaveButton.TabIndex = 4;
+            this.SaveButton.Text = "SAVE";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // MsgLabel
+            // 
+            this.MsgLabel.AutoSize = true;
+            this.MsgLabel.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MsgLabel.ForeColor = System.Drawing.Color.White;
+            this.MsgLabel.Location = new System.Drawing.Point(12, 23);
+            this.MsgLabel.Name = "MsgLabel";
+            this.MsgLabel.Size = new System.Drawing.Size(31, 13);
+            this.MsgLabel.TabIndex = 5;
+            this.MsgLabel.Text = "null";
+            // 
+            // YTThumbnailDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(70)))));
+            this.ClientSize = new System.Drawing.Size(934, 636);
+            this.Controls.Add(this.MsgLabel);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.DownloadButton);
+            this.Controls.Add(this.thumbBox);
+            this.Controls.Add(this.LinkBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(779, 599);
+            this.Name = "YTThumbnailDownloader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YT Thumbnail Downloader";
+            this.Load += new System.EventHandler(this.YTThumbnailDownloader_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.thumbBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox LinkBox;
+        private System.Windows.Forms.PictureBox thumbBox;
+        private System.Windows.Forms.Button DownloadButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Label MsgLabel;
+    }
+}
+
---
+++ YTThumbnailDownloader/YTTDownloader.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace YTThumbnailDownloader {
+    public partial class YTThumbnailDownloader : Form {
+        public YTThumbnailDownloader() {
+            InitializeComponent();
+        }
+
+        private void YTThumbnailDownloader_Load(object sender, EventArgs e) {
+            MsgLabel.Text = null;
+        }
+
+        bool isBusy = false;
+        private void DownloadButton_Click(object sender, EventArgs e) {
+            MsgLabel.Text = null;
+
+            if (!isBusy)
+                new System.Threading.Thread(DownloadThumbnail) { IsBackground = true }.Start();
+        }
+
+        private void LinkBox_KeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == Keys.Enter) {
+                DownloadButton_Click(sender, e);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        void DownloadThumbnail() {
+            if (isBusy)
+                return;
+            isBusy = true;
+
+            try {
+                string url = LinkBox.Text;
+                if (url.Contains("youtube.com") || url.Contains("youtu.be")) {
+                    byte[] html = null;
+                    html = new System.Net.WebClient().DownloadData(url);                   
+                    char[] htmlChars = Encoding.Default.GetString(html).ToCharArray();
+                    List<string> htmlLinks = new List<string>();
+
+                    string link = "";
+                    bool afterQuotes = false;
+                    foreach (char chr in htmlChars) {
+                        if (chr == '"') {
+                            afterQuotes = !afterQuotes;
+                            if (!afterQuotes) {
+                                htmlLinks.Add(link);
+                                link = "";
+                            }
+                        } else if (afterQuotes) {
+                            link += chr;
+                        }
+                    }
+
+                    foreach (string imageLink in htmlLinks) {
+                        if (imageLink.Contains("maxres") || imageLink.Contains("hqdefault")) {
+                            thumbBox.LoadAsync(imageLink);
+                        }
+                    }
+                } else
+                    PrintError(ErrorTypes.link);
+            }
+            catch (Exception) { PrintError(ErrorTypes.exception); isBusy = false; return; }
+
+            isBusy = false;
+        }
+       
+        enum ErrorTypes { save,link,exception }
+        void PrintError(ErrorTypes erType) {
+            this.Invoke((MethodInvoker)delegate {
+                switch (erType) {
+                    case ErrorTypes.save:
+                        MsgLabel.Text = "Can't save.";
+                        break;
+                    case ErrorTypes.link:
+                        MsgLabel.Text = "Try a youtube.com or youtu.be link instead.";
+                        break;
+                    case ErrorTypes.exception:
+                        MsgLabel.Text = "An error occurred.";
+                        break;
+                }
+            });        
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e) {
+            if (thumbBox.Image != null) {
+                MsgLabel.Text = null;
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                ImageFormat imgFormat = ImageFormat.Png;
+                sfd.FileName = "thumbnail";
+                sfd.Filter = "PNG (*.PNG)|*.png|JPG (*.JPG;*.JPEG)|*.jpg|All files(*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.OK) {
+                    thumbBox.Image.Save(sfd.FileName, imgFormat);
+                }
+            } else
+                PrintError(ErrorTypes.save);
+        }        
+    }
+}
